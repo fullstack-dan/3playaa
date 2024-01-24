8ea33208a3aef6f39910aa4af0bf1d27628d3f3a
fullstack-dan <aouloudan@icloud.com>
2024-01-23 19:26:34 -0500

    deploying online

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer.jsx'
+import Banner from './Components/Banner.jsx'
 
 function App() {
     return (
         <>
             <Navbar />
-            <main></main>
+            <main>
+                <Banner />
+            </main>
             <Footer />
         </>
     )
---
+++ src/Components/Banner.jsx
@@ -0,0 +1,10 @@
+import noNeed from '../assets/NO_NEED.png'
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <img src={noNeed} alt='banner' />
+        </div>
+    )
+}
+
+export default Banner
