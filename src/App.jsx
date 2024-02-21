f787444589f464d234d3a96ac8af5abd29f6b6af
fullstack-dan <aouloudan@icloud.com>
2024-02-21 16:17:42 -0500

    dev build

--- src/App.jsx
+++ src/App.jsx
@@ -17,101 +17,101 @@ import logo from './assets/3PLAYAA_SMALL.png'
 
 export const ShopContext = React.createContext()
 
-// function App() {
-//     const client = Client.buildClient({
-//         domain: '6bb8b0-da.myshopify.com',
-//         storefrontAccessToken: '2dc57f6555629f761c3b62ae161046db',
-//     })
-//
-//     return (
-//         <>
-//             <ShopContext.Provider value={client}>
-//                 <Router>
-//                     <Navbar />
-//                     <Routes>
-//                         <Route path='/' element={<HomePage />} />
-//                         <Route path='/shop' element={<ShopPage />} />
-//                         <Route path='/about' element={<AboutPage />} />
-//                         <Route path='/contact' element={<ContactPage />} />
-//                         <Route path='/products/:id' element={<ProductPage />} />
-//                     </Routes>
-//                     <Footer />
-//                 </Router>
-//             </ShopContext.Provider>
-//         </>
-//     )
-// }
-
 function App() {
-    const [timeLeft, setTimeLeft] = React.useState(calculateTimeLeft())
-
-    React.useEffect(() => {
-        const timer = setTimeout(() => {
-            setTimeLeft(calculateTimeLeft())
-        }, 1000)
-        return () => clearTimeout(timer)
-    })
-
-    function calculateTimeLeft() {
-        let year = new Date().getFullYear()
-        const difference =
-            +new Date(`02/29/${year >= 2024 ? year : 2024}`) - +new Date()
-        let timeLeft = {}
-
-        if (difference > 0) {
-            timeLeft = {
-                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                minutes: Math.floor((difference / 1000 / 60) % 60),
-                seconds: Math.floor((difference / 1000) % 60),
-            }
-        }
-
-        for (let key in timeLeft) {
-            if (timeLeft[key] === 0) {
-                timeLeft[key] = '0'
-            }
-        }
-
-        return timeLeft
-    }
-
-    const timerComponents = []
-
-    Object.keys(timeLeft).forEach((interval) => {
-        if (!timeLeft[interval]) {
-            return
-        }
-
-        timerComponents.push(
-            <div key={interval}>
-                <span>{timeLeft[interval]}</span>
-                <span>{interval}</span>
-            </div>
-        )
+    const client = Client.buildClient({
+        domain: '6bb8b0-da.myshopify.com',
+        storefrontAccessToken: '2dc57f6555629f761c3b62ae161046db',
     })
 
     return (
-        <main>
-            <div className='landing-page'>
-                <div className={'lp-logo-cont'}>
-                    <img
-                        src={'/assets/3PLAYAA.png'}
-                        alt='3Playaa'
-                        className='landing-page-logo'
-                    />
-                </div>
-                <div className={'countdown'}>
-                    {timerComponents.length ? (
-                        timerComponents
-                    ) : (
-                        <span>Coming soon...</span>
-                    )}
-                </div>
-                <EmailSignUp />
-            </div>
-        </main>
+        <>
+            <ShopContext.Provider value={client}>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route path='/' element={<HomePage />} />
+                        <Route path='/shop' element={<ShopPage />} />
+                        <Route path='/about' element={<AboutPage />} />
+                        <Route path='/contact' element={<ContactPage />} />
+                        <Route path='/products/:id' element={<ProductPage />} />
+                    </Routes>
+                    <Footer />
+                </Router>
+            </ShopContext.Provider>
+        </>
     )
 }
+
+// function App() {
+//     const [timeLeft, setTimeLeft] = React.useState(calculateTimeLeft())
+//
+//     React.useEffect(() => {
+//         const timer = setTimeout(() => {
+//             setTimeLeft(calculateTimeLeft())
+//         }, 1000)
+//         return () => clearTimeout(timer)
+//     })
+//
+//     function calculateTimeLeft() {
+//         let year = new Date().getFullYear()
+//         const difference =
+//             +new Date(`02/29/${year >= 2024 ? year : 2024}`) - +new Date()
+//         let timeLeft = {}
+//
+//         if (difference > 0) {
+//             timeLeft = {
+//                 days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+//                 hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+//                 minutes: Math.floor((difference / 1000 / 60) % 60),
+//                 seconds: Math.floor((difference / 1000) % 60),
+//             }
+//         }
+//
+//         for (let key in timeLeft) {
+//             if (timeLeft[key] === 0) {
+//                 timeLeft[key] = '0'
+//             }
+//         }
+//
+//         return timeLeft
+//     }
+//
+//     const timerComponents = []
+//
+//     Object.keys(timeLeft).forEach((interval) => {
+//         if (!timeLeft[interval]) {
+//             return
+//         }
+//
+//         timerComponents.push(
+//             <div key={interval}>
+//                 <span>{timeLeft[interval]}</span>
+//                 <span>{interval}</span>
+//             </div>
+//         )
+//     })
+//
+//     return (
+//         <main>
+//             <div className='landing-page'>
+//                 <div className={'lp-logo-cont'}>
+//                     <img
+//                         src={'/assets/3PLAYAA.png'}
+//                         alt='3Playaa'
+//                         className='landing-page-logo'
+//                     />
+//                 </div>
+//                 <div className={'countdown'}>
+//                     {timerComponents.length ? (
+//                         timerComponents
+//                     ) : (
+//                         <span>Coming soon...</span>
+//                     )}
+//                 </div>
+//                 <EmailSignUp />
+//             </div>
+//         </main>
+//     )
+// }
 
 export default App
